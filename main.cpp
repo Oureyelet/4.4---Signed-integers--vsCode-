b25e02ccbe6c80944aeaab87793119a58ecb697b
Mateusz Kacperski <oureyelet@gmail.com>
2021-02-23 20:28:53 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "first.h"
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    
+    short s;
+    int i;
+    long l;
+    long long ll;
+
+    float x{8};
+    float y{5};
+    
+
+    cout << (x/y) << '\n' << endl;
+
+    return 0;
+}
